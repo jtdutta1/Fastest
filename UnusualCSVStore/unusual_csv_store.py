72dd223bf44823403c8a47eef541447f9fa3e378
Jeet Dutta <jtdutta1@gmail.com>
2021-05-07 11:23:06 +0530

    Added add_data, printing and repr functionality

--- UnusualCSVStore/unusual_csv_store.py
+++ UnusualCSVStore/unusual_csv_store.py
@@ -1,14 +1,18 @@
 import csv
 class UnusualCSVStore:
     """
+    v0.8
     This is currently in a bare form without any exception handling.
     """
     def __init__(self, data=None, filename=None):
-        if data is not None:
-            self.__data__ = data
+        if data is not None:            
+            self.__header__ = data[0]
+            self.__data__ = data[1:]
         elif filename is not None:
             self.__filename__ = filename
-            self.__data__ = self.__load_data__()
+            data = self.__load_data__()
+            self.__header__ = data[0]
+            self.__data__ = data[1:]
     
     def __load_data__(self):
         with open(self.__filename__, 'r') as foo:
@@ -17,21 +21,29 @@ class UnusualCSVStore:
     
     def store_data(self, filename="temp.csv", file_mode="w+"):
         with open(filename, file_mode) as foo:
-            csvwriter = csv.writer(foo, delimiter=',')
-            csvwriter.writerow(self.__data__[0])
-            temp = []
-            nheaders = len(self.__data__[0])
-            count_headers = 0
-            for i in self.__data__[1:]:
-                l = [str(j) for j in i]
-                s = ' '.join(l)
-                s = '[' + s + ']'
-                temp.append(s)
-                count_headers += 1
-                if count_headers == nheaders:
-                    csvwriter.writerow(temp)
-                    temp = []
-                    count_headers = 0
+            foo.write(self.__repr__())
     
-    # def add_data(self, data):
-# 
+    def add_data(self, data):
+        assert(len(data) == len(self.__header__))
+        self.__data__.extend(data)
+    
+    def __repr__(self):
+        data = ''
+        data += ','.join(self.__header__) + '\n'
+        temp = []
+        nheaders = len(self.__header__)
+        count_headers = 0
+        for i in self.__data__:
+            l = [str(j) for j in i]
+            s = ' '.join(l)
+            s = '[' + s + ']'
+            temp.append(s)
+            count_headers += 1
+            if count_headers == nheaders:
+                data += ','.join(temp) + '\n'
+                temp = []
+                count_headers = 0
+        return data
+
+    def __str__(self):
+        return self.__repr__()
